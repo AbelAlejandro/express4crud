d25fb8cd024c682340045b4fb965ac3452510522
AbelAlejandro <abel.alejandro@ymail.com>
2014-11-16 12:15:28 -0300

    Changes

--- models/persons.js
+++ models/persons.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var personSchema = new Schema({
+var clienteSchema = new Schema({
     name: String,
     age: Number
 });
 
-var personModel = mongoose.model('Persons', personSchema);
+var clienteModel = mongoose.model('Clientes', clienteSchema);
 
-module.exports = personModel;
+module.exports = clienteModel;
